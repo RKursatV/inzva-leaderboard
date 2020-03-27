895b202a359c7d625f3e27435090cc739d3b8023
R. Kürşat Vuruşan <kursat.vurusan@ieee.metu.edu.tr>
2020-03-27 10:14:39 +0300

    general view changed, rank added

--- data_prettify/datacon.py
+++ data_prettify/datacon.py
@@ -6,10 +6,14 @@ with open('data.json') as json_file:
 
 newdata = []
 data = OrderedDict(sorted(data.items(), key=lambda x: (x[1]["Rating"],sorted(x[1]["Rating History"].keys())[-1]),reverse=True))
-
+rank,val = 0,1337
 for index,i in enumerate(data):
     data[i]["Username"] = i
-    data[i]["rank"] = index+1
+    if(data[i]["Rating"]) < val:
+        rank += 1
+        val = data[i]["Rating"]
+    data[i]["rank"] = rank
     newdata.append(data[i])
 with open('newdata.json', 'w') as f:
     json.dump(newdata, f, ensure_ascii=False)
+
