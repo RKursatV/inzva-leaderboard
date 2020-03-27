c40e8d67d24a1fb5b15b41a8ac58e96b5d30e7cb
R. Kürşat Vuruşan <kursat.vurusan@ieee.metu.edu.tr>
2020-03-27 09:53:02 +0300

    ranks added

--- data_prettify/datacon.py
+++ data_prettify/datacon.py
@@ -1,10 +1,15 @@
 import json
+from collections import OrderedDict
+
 with open('data.json') as json_file:
     data = json.load(json_file)
 
 newdata = []
+data = OrderedDict(sorted(data.items(), key=lambda x: (x[1]["Rating"],sorted(x[1]["Rating History"].keys())[-1]),reverse=True))
+
 for index,i in enumerate(data):
     data[i]["Username"] = i
+    data[i]["rank"] = index+1
     newdata.append(data[i])
 with open('newdata.json', 'w') as f:
     json.dump(newdata, f, ensure_ascii=False)
